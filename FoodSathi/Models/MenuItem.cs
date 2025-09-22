ae8408d78ed9c3817561707258ddb7ed2dbfff64
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-09-22 20:10:24 +0545

    adding menu page

---
+++ FoodSathi/Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodSathi.Models
+{
+    public class MenuItem
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
