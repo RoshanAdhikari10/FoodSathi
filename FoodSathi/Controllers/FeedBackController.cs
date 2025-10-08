ffd97046c8fca68e3dcf8b1e0d019ad316fdb739
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-08 10:27:56 +0545

    Adding design

---
+++ FoodSathi/Controllers/FeedBackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodSathi.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        public IActionResult feedback()
+        {
+            return View();
+        }
+    }
+}
