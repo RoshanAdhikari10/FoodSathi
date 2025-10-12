6de9bc50aa21ba4f84143f83d782408903608781
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-12 18:47:36 +0545

    Add feedback controller

--- FoodSathi/Controllers/FeedBackController.cs
+++ FoodSathi/Controllers/FeedBackController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using FoodSathi.Data;
+using FoodSathi.Models;
+using System.Linq;
 
 namespace FoodSathi.Controllers
 {
-    public class FeedBackController : Controller
+    public class FeedbackController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public FeedbackController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Display Feedback page
         public IActionResult feedback()
         {
-            return View();
+            var feedbacks = _context.Feedbacks.OrderByDescending(f => f.Date).ToList();
+            ViewBag.AvgRating = feedbacks.Count > 0 ? feedbacks.Average(f => f.Rating) : 0;
+            return View(feedbacks);
+        }
+
+        // Submit feedback
+        [HttpPost]
+        public IActionResult SubmitFeedback(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Feedbacks.Add(feedback);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
         }
     }
 }
+
