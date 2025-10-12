1d34eb406380fe12daa063152c2fc2c49d3d9e8f
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-12 18:46:34 +0545

    Add feedback

---
+++ FoodSathi/Models/Feedback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodSathi.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Review { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
