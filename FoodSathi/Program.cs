0603e0455d84c7e44a2ebb8fa71dfe99d80bd0b8
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-15 13:34:00 +0545

    chnages

--- FoodSathi/Program.cs
+++ FoodSathi/Program.cs
@@ -3,66 +3,44 @@ using FoodSathi.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+var builder = WebApplication.CreateBuilder(args); // Add services to the container.
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDbContext<MenuDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Menupage")));
-
-
-
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString)); 
+builder.Services.AddDbContext<MenuDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Menupage"))); builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => {
-    options.SignIn.RequireConfirmedAccount = true;
-   // Password settings
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 4;
-})
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
+options.SignIn.RequireConfirmedAccount = true; 
+//Password settings
+options.Password.RequireDigit = false;
+options.Password.RequireLowercase = false;
+options.Password.RequireUppercase = false;
+options.Password.RequireNonAlphanumeric = false;
+options.Password.RequiredLength = 4; })
+.AddEntityFrameworkStores<ApplicationDbContext>(); 
+builder.Services.AddControllersWithViews(); 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) 
 {
-    app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint(); 
 }
-else
-{
+else {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); 
 }
-
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
-app.MapControllerRoute(
-    name: "checkoutByName",
-    pattern: "Order/Checkout/{orderName}",
-    defaults: new { controller = "Order", action = "Checkout" });
-
-
+    app.MapControllerRoute(
+        name: "checkoutByName", pattern: "Order/Checkout/{orderName}",
+        defaults: new { controller = "Order", action = "Checkout" });
 app.MapRazorPages()
-   .WithStaticAssets();
-
+    .WithStaticAssets();
 app.Run();
