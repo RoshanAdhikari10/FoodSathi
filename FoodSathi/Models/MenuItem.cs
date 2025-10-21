2988b3184a9ab44fadc74151107855078340723f
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-21 11:28:52 +0545

    changes

--- FoodSathi/Models/MenuItem.cs
+++ FoodSathi/Models/MenuItem.cs
@@ -1,27 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace FoodSathi.Models
 {
     public class MenuItem
     {
-        [Key] // Primary Key
+        [Key]
         public int ItemID { get; set; }
 
-        [Required] // cannot be null
-        [StringLength(100)] // max length
+        [Required, StringLength(100)]
         public string ItemName { get; set; }
 
         [StringLength(500)]
         public string Description { get; set; }
 
-        [Range(1, 10000)] // valid price range
+        [Range(1, 10000)]
         public decimal Price { get; set; }
 
-        public string ImageURL { get; set; } // store direct image links
+        public string? ImagePath { get; set; }
+
+        [Required]
+        public string Category { get; set; }
 
         public bool IsAvailable { get; set; } = true;
 
-        [Required]
-        public string Category { get; set; } // Pizza, Burger, Drinks, Desserts
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
     }
 }
