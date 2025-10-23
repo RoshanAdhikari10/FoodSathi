3a2fe639df426cfa5b5f6c728147ee934a616d66
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-23 06:33:31 +0545

    added

---
+++ FoodSathi/Models/CheckoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FoodSathi.Models
+{
+    public class CheckoutViewModel
+    {
+        public bool FromCart { get; set; }                  // true if from cart
+        public List<Cart>? CartItems { get; set; }          // for multiple items
+        public Order? SingleOrder { get; set; }             // for single order
+        public decimal TotalAmount { get; set; }            // total price
+    }
+}
