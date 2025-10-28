93ae1cf4e7e381343d47b12130d9814d1a087a80
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-28 15:22:14 +0545

    modify

--- FoodSathi/Controllers/MenuItemsController.cs
+++ FoodSathi/Controllers/MenuItemsController.cs
@@ -3,26 +3,40 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FoodSathi.Controllers
 {
     public class MenuItemsController : Controller
     {
         private readonly MenuDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public MenuItemsController(MenuDbContext context)
+        // SINGLE CONSTRUCTOR - Remove any duplicate constructors!
+        public MenuItemsController(MenuDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET: MenuItems
         public async Task<IActionResult> Index()
         {
-            return View(await _context.MenuItems.ToListAsync());
+            var categories = await _context.MenuItems
+                .Select(m => m.Category)
+                .Distinct()
+                .ToListAsync();
+
+            var items = await _context.MenuItems.ToListAsync();
+
+            ViewBag.Categories = categories;
+
+            return View(items);
         }
 
         // GET: MenuItems/Details/5
@@ -42,22 +56,47 @@ namespace FoodSathi.Controllers
 
             return View(menuItem);
         }
+
         [Authorize(Roles = "Admin")]
         // GET: MenuItems/Create
         public IActionResult Create()
         {
             return View();
         }
+
         [Authorize(Roles = "Admin")]
         // POST: MenuItems/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ItemID,ItemName,Description,Price,ImagePath,Category,IsAvailable")] MenuItem menuItem)
+        public async Task<IActionResult> Create([Bind("ItemID,ItemName,Description,Price,ImageFile,Category,IsAvailable")] MenuItem menuItem)
         {
             if (ModelState.IsValid)
             {
+                // Handle image upload
+                if (menuItem.ImageFile != null)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+
+                    // Create directory if it doesn't exist
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    // Generate unique filename
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + menuItem.ImageFile.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    // Save file to wwwroot/images
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await menuItem.ImageFile.CopyToAsync(fileStream);
+                    }
+
+                    // Store relative path in database
+                    menuItem.ImagePath = "/images/" + uniqueFileName;
+                }
+
                 _context.Add(menuItem);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -81,13 +120,12 @@ namespace FoodSathi.Controllers
             }
             return View(menuItem);
         }
+
         [Authorize(Roles = "Admin")]
         // POST: MenuItems/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ItemID,ItemName,Description,Price,ImagePath,Category,IsAvailable")] MenuItem menuItem)
+        public async Task<IActionResult> Edit(int id, [Bind("ItemID,ItemName,Description,Price,ImageFile,Category,IsAvailable")] MenuItem menuItem)
         {
             if (id != menuItem.ItemID)
             {
@@ -98,6 +136,46 @@ namespace FoodSathi.Controllers
             {
                 try
                 {
+                    // Get existing item to preserve old image path if no new image uploaded
+                    var existingItem = await _context.MenuItems.AsNoTracking().FirstOrDefaultAsync(m => m.ItemID == id);
+
+                    // Handle new image upload
+                    if (menuItem.ImageFile != null)
+                    {
+                        // Delete old image if exists
+                        if (!string.IsNullOrEmpty(existingItem?.ImagePath))
+                        {
+                            string oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, existingItem.ImagePath.TrimStart('/'));
+                            if (System.IO.File.Exists(oldImagePath))
+                            {
+                                System.IO.File.Delete(oldImagePath);
+                            }
+                        }
+
+                        // Upload new image
+                        string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+
+                        if (!Directory.Exists(uploadsFolder))
+                        {
+                            Directory.CreateDirectory(uploadsFolder);
+                        }
+
+                        string uniqueFileName = Guid.NewGuid().ToString() + "_" + menuItem.ImageFile.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await menuItem.ImageFile.CopyToAsync(fileStream);
+                        }
+
+                        menuItem.ImagePath = "/images/" + uniqueFileName;
+                    }
+                    else
+                    {
+                        // Keep old image path if no new image uploaded
+                        menuItem.ImagePath = existingItem?.ImagePath;
+                    }
+
                     _context.Update(menuItem);
                     await _context.SaveChangesAsync();
                 }
@@ -116,6 +194,7 @@ namespace FoodSathi.Controllers
             }
             return View(menuItem);
         }
+
         [Authorize(Roles = "Admin")]
         // GET: MenuItems/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -134,6 +213,7 @@ namespace FoodSathi.Controllers
 
             return View(menuItem);
         }
+
         [Authorize(Roles = "Admin")]
         // POST: MenuItems/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -143,6 +223,16 @@ namespace FoodSathi.Controllers
             var menuItem = await _context.MenuItems.FindAsync(id);
             if (menuItem != null)
             {
+                // Delete image file if exists
+                if (!string.IsNullOrEmpty(menuItem.ImagePath))
+                {
+                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, menuItem.ImagePath.TrimStart('/'));
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+
                 _context.MenuItems.Remove(menuItem);
             }
 
