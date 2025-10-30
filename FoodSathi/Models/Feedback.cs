26d936972a43d225d70b574bf0c16f599da9b3e1
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-10-30 08:33:33 +0545

    modify

--- FoodSathi/Models/Feedback.cs
+++ FoodSathi/Models/Feedback.cs
@@ -5,24 +5,36 @@ namespace FoodSathi.Models
 {
     public class Feedback
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
         [StringLength(100)]
-        public string UserName { get; set; }
+        public string UserId { get; set; }  // From logged-in user
 
         [Required]
         [StringLength(100)]
+        public string Name { get; set; }  // From logged-in user
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }  // From logged-in user
+
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(200)]
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
 
-        [Required]
-        [Range(1, 5)]
+        [Required(ErrorMessage = "Rating is required")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public int Rating { get; set; }
 
-        [Required]
-        [StringLength(500)]
-        public string Review { get; set; }
+        [Required(ErrorMessage = "Feedback message is required")]
+        [StringLength(1000)]
+        [Display(Name = "Your Feedback")]
+        public string Message { get; set; }
 
-        public DateTime Date { get; set; } = DateTime.Now;
+        public DateTime SubmittedDate { get; set; } = DateTime.Now;
     }
 }
