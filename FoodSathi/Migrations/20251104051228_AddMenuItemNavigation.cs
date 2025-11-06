4aa9b80deee751ebf450fbea656a60b8eab28044
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-11-06 17:47:37 +0545

    added

---
+++ FoodSathi/Migrations/20251104051228_AddMenuItemNavigation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodSathi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMenuItemNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ItemID",
+                table: "Orders",
+                column: "ItemID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_MenuItems_ItemID",
+                table: "Orders",
+                column: "ItemID",
+                principalTable: "MenuItems",
+                principalColumn: "ItemID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_MenuItems_ItemID",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ItemID",
+                table: "Orders");
+        }
+    }
+}
