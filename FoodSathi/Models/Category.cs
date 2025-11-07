b6030e9c3258adce3240fe184801750fed2d07ce
RoshanAdhikari10 <chhetrirosun@gmail.com>
2025-11-07 12:49:36 +0545

    added

---
+++ FoodSathi/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodSathi.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+    }
+}
